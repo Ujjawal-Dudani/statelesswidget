1357abbeafa4dd5fcbc29441ca4721f4cd57f33e
Ujjawal-Dudani <89912402+Ujjawal-Dudani@users.noreply.github.com>
2021-12-06 12:01:18 +0530

    statelesswidget

---
+++ lib/mywidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32.0),
+      child: const Text('Hello World'),
+    );
+  }
+}
